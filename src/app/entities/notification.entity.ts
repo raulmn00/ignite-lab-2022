39c8ad548766b7d307020c885d8c25da3c870b0c
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-12-15 11:08:02 -0300

    feat: abstract content type to notificationContent class

--- src/app/entities/notification.entity.ts
+++ src/app/entities/notification.entity.ts
@@ -1,9 +1,49 @@
-export class NotificationEntity {
-  private content: string;
-  private category: string;
+import { NotificationContent } from './notificationContent';
 
-  constructor({ content, category }) {
-    this.category = category;
-    this.content = content;
+export interface NotificationProps {
+  recipientId: string;
+  content: NotificationContent;
+  category: string;
+  readAt?: Date | null;
+  createdAt: Date;
+}
+
+export class NotificationEntity {
+  private props: NotificationProps;
+
+  constructor(props: NotificationProps) {
+    this.props = props;
+  }
+
+  public set recipientId(recipientId: string) {
+    this.props.recipientId = recipientId;
+  }
+  public get recipientId(): string {
+    return this.props.recipientId;
+  }
+
+  public set content(content: NotificationContent) {
+    this.props.content = content;
+  }
+  public get content(): NotificationContent {
+    return this.props.content;
+  }
+
+  public set category(category: string) {
+    this.props.category = category;
+  }
+  public get category(): string {
+    return this.props.category;
+  }
+
+  public set readAt(readAt: Date | null | undefined) {
+    this.props.readAt = readAt;
+  }
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
   }
 }
---
+++ src/app/entities/notificationContent.ts
@@ -0,0 +1,18 @@
+export class NotificationContent {
+  private readonly content: string;
+  constructor(content: string) {
+    const isContentLengthValid = this.validateContentLength(content);
+    if (!isContentLengthValid) {
+      throw new Error('Content length error.');
+    } else {
+      this.content = content;
+    }
+  }
+  get value(): string {
+    return this.content;
+  }
+
+  private validateContentLength(content: string): boolean {
+    return content.length >= 5 && content.length <= 240;
+  }
+}
